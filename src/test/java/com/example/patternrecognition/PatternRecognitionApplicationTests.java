e774817a3f1def4e13f6baad3b9bf5dd57780ec6
sinol-italy <sinol.italy@gmail.com>
2024-02-24 12:59:51 +0100

    init

---
+++ src/test/java/com/example/patternrecognition/PatternRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patternrecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
